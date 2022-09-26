5f4aafb071d921829bab408b88068ed050ba7b11
Hyemm <48474929+hm5938@users.noreply.github.com>
2022-09-26 21:11:27 +0900

    feature #1 - appication 생성

---
+++ src/main/java/com/search/statutesearching/StatuteSearchingApplication.java
@@ -0,0 +1,13 @@
+package com.search.statutesearching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatuteSearchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatuteSearchingApplication.class, args);
+	}
+
+}
