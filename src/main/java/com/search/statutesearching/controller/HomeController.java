23a8e5e24f4534c295452c35103f7c1d299a71bb
Cho-El <syzzang21c@gmail.com>
2022-09-27 18:43:49 +0900

    기본세팅

---
+++ src/main/java/com/search/statutesearching/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.search.statutesearching.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/home", method= RequestMethod.GET)
+    public String goHome(HttpServletRequest request) {
+        return "content/home";
+    }
+}
