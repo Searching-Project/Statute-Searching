ee9330635415a913c6f0d14582b188bd6969bace
Hyemm <48474929+hm5938@users.noreply.github.com>
2022-09-27 23:55:57 +0900

    feature #4 - article 검색 쿼리 작성

--- src/main/java/com/search/statutesearching/repository/ArticleRepository.java
+++ src/main/java/com/search/statutesearching/repository/ArticleRepository.java
@@ -2,7 +2,24 @@ package com.search.statutesearching.repository;
 
 import com.search.statutesearching.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article,Long> {
-}
+    //like or 사용
+    @Query(value = "select * FROM ARTICLE as a WHERE a.content like %:keyword% OR a.title like %:keyword%",nativeQuery = true )
+    List<Article> search(@Param("keyword")String keyword);
+    //like union 사용
+    @Query(value = "select * FROM ARTICLE as a WHERE a.content like %:keyword% " +
+            "union select * FROM ARTICLE as a WHERE a.title like %:keyword% ",nativeQuery = true )
+    List<Article> search2(@Param("keyword")String keyword);
 
+    //N-gram parser
+    @Query(value = "(select * FROM ARTICLE as a WHERE MATCH(a.title) AGAINST(:keyword IN BOOLEAN MODE))" +
+            "union (select * FROM ARTICLE as a WHERE MATCH(a.content) AGAINST(:keyword IN BOOLEAN MODE))",nativeQuery = true )
+    List<Article> search3(@Param("keyword")String keyword);
+
+
+}
---
+++ src/main/java/com/search/statutesearching/service/ArticleService.java
@@ -0,0 +1,64 @@
+package com.search.statutesearching.service;
+
+import com.search.statutesearching.dto.reponse.ResponseDto;
+import com.search.statutesearching.entity.Article;
+import com.search.statutesearching.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional
+    public ResponseDto<?> search(String keyword){
+        List<Article> articleList = articleRepository.search(keyword);
+        return ResponseDto.success(articleList);
+        //throw new CustomException(RESULT_NOT_FOUND);
+    }
+    @Transactional
+    public ResponseDto<?> search2(String keyword) {
+        List<Article> articleList = articleRepository.search2(keyword);
+        return ResponseDto.success(articleList);
+    }
+
+    @Transactional
+    public ResponseDto<?> search3(String keyword) {
+        keyword = getAnd(keyword);
+        List<Article> articleList = articleRepository.search3(keyword);
+        return ResponseDto.success(articleList);
+    }
+
+
+    //"검색어1" OR "검색어2"가 포함된 데이터를 조회
+    public String getOr(String str){
+        return str;
+    }
+
+    //"검색어1" AND "검색어2"가 모두 포함된 데이터를 조회
+    public String getAnd(String str){
+        return "+"+str.replace(" "," +");
+    }
+
+    //"검색어1"이 포함된 데이터 중에 "검색어2"가 들어간 것 제외하여 조회
+    public String getDifference(String str){
+        return str.replace(" ", " -");
+    }
+
+    //"검색어%"가 포함된 데이터를 조회 ("%검색어" 조회는 안되는듯?)
+    public String getStartWith(String str){
+        return str+'*';
+    }
+
+    //"검색어1 검색어2"가 포함된 데이터를 조회
+    public String getSentence(String str){
+        return "\""+str+"\"";
+    }
+}
