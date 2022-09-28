df0528df44047b9f97734e61de0e6d119608248a
Hyemm <48474929+hm5938@users.noreply.github.com>
2022-09-29 02:31:50 +0900

    feature #8 검색기능을 위한 ResponseDto 생성

---
+++ src/main/java/com/search/statutesearching/dto/reponse/SearchResDto.java
@@ -0,0 +1,17 @@
+package com.search.statutesearching.dto.reponse;
+
+public interface SearchResDto {
+
+    String getLawsn();
+    String getKoreanName();
+    String getShorterName();
+    String getContent();
+    String getTitle();
+    Long getAArticleId();
+    Long getPArticleId();
+    Long getPParagraphId();
+    String getParagraphContent();
+    Long getHParagraphId();
+    String getHoContent();
+
+}
