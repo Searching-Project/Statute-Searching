7d51a53569691fb5e97ac7d951c9eb53a1dfefe7
송민진 <m_6595@naver.com>
2022-09-28 11:40:11 +0900

    판례 검색 성능 테스트

--- src/main/java/com/search/statutesearching/repository/PrecedentRepository.java
+++ src/main/java/com/search/statutesearching/repository/PrecedentRepository.java
@@ -2,7 +2,27 @@ package com.search.statutesearching.repository;
 
 
 import com.search.statutesearching.entity.Precedent;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PrecedentRepository extends JpaRepository<Precedent, Long> {
+    List<Precedent> findAllByCaseNameContaining(String keyword);
+
+    @Query("SELECT p FROM Precedent p WHERE p.caseName LIKE %:keyword%")
+    List<Precedent> searchByCaseNameLike(@Param("keyword")String keyword);
+
+
+    @Query(value = "SELECT * FROM precedent WHERE MATCH(case_name, content, judge_holding, judge_reasoning)"
+            + "AGAINST ('keyword' IN NATURAL LANGUAGE MODE)", nativeQuery = true)
+    Page<Precedent> fullTextSearch(String keyword, Pageable pageable);
+
+
+    @Query(value = "SELECT * FROM precedent WHERE MATCH(case_name, content, judge_holding, judge_reasoning)"
+            + "AGAINST (':keyword' IN NATURAL LANGUAGE MODE)", nativeQuery = true)
+    Page<Precedent> fullTextSearchNgram(@Param("keyword")String keyword, Pageable pageable);
 }
---
+++ src/test/java/com/search/statutesearching/PrecedentIndexTest.java
@@ -0,0 +1,79 @@
+package com.search.statutesearching;
+
+import com.search.statutesearching.repository.PrecedentRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.util.StopWatch;
+
+@SpringBootTest
+public class PrecedentIndexTest {
+
+    @Autowired
+    private PrecedentRepository precedentRepository;
+
+    @DisplayName("JPA - contains")
+    @Test
+    void jpa_contains_Test() {
+        // given
+        StopWatch stopWatch = new StopWatch();
+
+        // when
+        stopWatch.start();
+        precedentRepository.findAllByCaseNameContaining("부당해고");
+        stopWatch.stop();
+
+        // then
+        System.out.println(stopWatch.prettyPrint());
+    }
+
+    @DisplayName("Native @Query like")
+    @Test
+    void like_Test() {
+        // given
+        StopWatch stopWatch = new StopWatch();
+
+        // when
+        stopWatch.start();
+        precedentRepository.findAllByCaseNameContaining("부당해고");
+        stopWatch.stop();
+
+        // then
+        System.out.println(stopWatch.prettyPrint());
+    }
+
+    @DisplayName("Full Text Search")
+    @Test
+    void fullTextSearch_Test() {
+        // given
+        Pageable pageable = PageRequest.of(1, 10);
+        StopWatch stopWatch = new StopWatch();
+
+        // when
+        stopWatch.start();
+//        precedentRepository.fullTextSearch("부당해고", pageable);
+        stopWatch.stop();
+
+        // then
+        System.out.println(stopWatch.prettyPrint());
+    }
+
+    @DisplayName("Full Text Search - nGram")
+    @Test
+    void fullTextSearch_ngram_Test() {
+        // given
+        Pageable pageable = PageRequest.of(10, 100);
+        StopWatch stopWatch = new StopWatch();
+
+        // when
+        stopWatch.start();
+        precedentRepository.fullTextSearchNgram("부당해고", pageable);
+        stopWatch.stop();
+
+        // then
+        System.out.println(stopWatch.prettyPrint());
+    }
+}
